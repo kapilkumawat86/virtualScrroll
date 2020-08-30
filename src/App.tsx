4ca41d03f92bb352b15023a317bee7565d8189bd
Kapil Kumawat <erkapil2006@gmail.com>
2020-08-30 11:55:58 +0530

    Minor change

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,7 @@ const rowTemplate = (item: Data) => (
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Virtual Scroll</h1>
       <VirtualScoller<Data>
         settings={SETTINGS}
         getData={getData}
